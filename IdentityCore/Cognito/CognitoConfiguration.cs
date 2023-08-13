051badf6a94914d1be87d944e285e797adb8f03a
Phan Xuân Chánh <contact@phanxuanchanh.com>
2023-08-13 13:57:31 +0700

    Added cognito identity

---
+++ IdentityCore/Cognito/CognitoConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityCore.Cognito;
+
+public class CognitoConfiguration
+{
+    public string? Region { get; set; }
+    public string? UserPoolClientId { get; set; }
+    public string? UserPoolClientSecret { get; set; }
+    public string? UserPoolId { get; set; }
+}
--- IdentityCore/DependencyInjection/ServiceRegistration.cs
+++ IdentityCore/DependencyInjection/ServiceRegistration.cs
@@ -1,5 +1,8 @@
-﻿using Amazon.AspNetCore.Identity.Cognito;
+﻿using Amazon;
+using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.CognitoIdentityProvider;
 using Amazon.Extensions.CognitoAuthentication;
+using IdentityCore.Cognito;
 using Japanese.LanguageCore.Identity;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
@@ -12,6 +15,20 @@ public static class ServiceRegistration
 {
     public static IServiceCollection AddAmazonCognito(this IServiceCollection services, IConfiguration configuration)
     {
+        CognitoConfiguration cognitoConfiguration = configuration.GetSection("AmazonCognito")
+            .Get<CognitoConfiguration>();
+
+        AmazonCognitoIdentityProviderConfig cognitoIdentityProviderConfig = new AmazonCognitoIdentityProviderConfig
+        {
+            RegionEndpoint = RegionEndpoint.GetBySystemName(cognitoConfiguration.Region)
+        };
+
+        AmazonCognitoIdentityProviderClient cognitoIdentityProvider = new AmazonCognitoIdentityProviderClient(cognitoIdentityProviderConfig);
+        CognitoUserPool cognitoUserPool = new CognitoUserPool(cognitoConfiguration.UserPoolId, cognitoConfiguration.UserPoolClientId, cognitoIdentityProvider, clientSecret: cognitoConfiguration.UserPoolClientSecret);
+
+        services.AddSingleton<IAmazonCognitoIdentityProvider>(cognitoIdentityProvider);
+        services.AddSingleton(cognitoUserPool);
+
         services.AddCognitoIdentity();
         services.AddAuthentication(options =>
         {
@@ -20,7 +37,7 @@ public static class ServiceRegistration
         })
         .AddJwtBearer(options =>
         {
-            options.Authority = configuration["AWSCognito:Authority"];
+            //options.Authority = configuration["AmazonCognito"];
             options.TokenValidationParameters = new TokenValidationParameters
             {
                 ValidateIssuerSigningKey = true,
